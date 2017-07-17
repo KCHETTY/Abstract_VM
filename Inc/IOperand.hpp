c227dcd24a238a577ab4d381568bd2a8632cb0e6
KCHETTY <kchetty416@gmail.com>
2017-07-17 04:28:25 +0200

    hdhd

--- Inc/IOperand.hpp
+++ Inc/IOperand.hpp
@@ -4,16 +4,17 @@
 #include <iostream>
 #include <stdint.h>
 #include <math.h>
+#include <deque>
 
 enum eOperandType { INT8, INT16, INT32, FLOAT, DOUBLE};
 
 class IOperand
 {
     public:
-        //virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
+        virtual int getPrecision( void ) const = 0; // Precision of the type of the instance
         virtual eOperandType getType( void ) const = 0; // Type of the instance
 
-        /*virtual IOperand const * operator+( IOperand const & obj ) = 0; // Sum
+        virtual IOperand const * operator+( IOperand const & obj ) = 0; // Sum
         virtual IOperand const * operator-( IOperand const & obj ) = 0; // Difference
         virtual IOperand const * operator*( IOperand const & obj ) = 0; // Product
         virtual IOperand const * operator/( IOperand const & obj ) = 0; // Quotient
--- Inc/Operand.hpp
+++ Inc/Operand.hpp
@@ -5,27 +5,94 @@
 template <eOperandType type, class T> class Operand : public IOperand
 {
     private:
-        T num_value;
         std::string str_value;
 
         Operand();
 
     public:
 
+        T num_value;
+
         Operand( T tmp_val , std::string str_tmp_val );
 
-        //int getPrecision( void ) const; // Precision of the type of the instance
+        int getPrecision( void ) const; // Precision of the type of the instance
         eOperandType getType( void ) const; // Type of the instance
 
-        /*IOperand const * operator+( IOperand const & obj ); // Sum
+        IOperand const * create_obj(eOperandType type_tmp, long double value);
+
+        IOperand const * operator+( IOperand const & obj ); // Sum
         IOperand const * operator-( IOperand const & obj ); // Difference
         IOperand const * operator*( IOperand const & obj ); // Product
         IOperand const * operator/( IOperand const & obj ); // Quotient
         IOperand const * operator%( IOperand const & obj ); // Modulo
 
-        std::string const & toString( void ) const; // String representation of the instance*/
+        std::string const & toString( void ) const; // String representation of the instance
         ~Operand( void ) {}
 };
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::create_obj(eOperandType type_tmp, long double value)
+{
+    switch (type_tmp)
+    {
+        case eOperandType::INT8:
+            return ( new Operand<eOperandType::INT8, int8_t>::Operand( static_cast<int8_t> (value), std::to_string(value) ) );
+        case eOperandType::INT16:
+            return ( new Operand<eOperandType::INT16, int16_t>::Operand( static_cast<int16_t> (value), std::to_string(value) ) );
+        case eOperandType::INT32:
+            return ( new Operand<eOperandType::INT32, int32_t>::Operand( static_cast<int32_t> (value), std::to_string(value) ) );
+        case eOperandType::FLOAT:
+            return ( new Operand<eOperandType::FLOAT, float_t>::Operand( static_cast<float_t> (value) , std::to_string(value) ));
+        case eOperandType::DOUBLE:
+            return ( new Operand<eOperandType::DOUBLE, double_t>::Operand( static_cast<double_t> (value), std::to_string(value)));
+    }
+
+    return (NULL);
+}
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::operator+( IOperand const & obj )
+{
+    long double tmp = std::stold(this->str_value) + std::stold(obj.toString());
+
+    if (obj.getPrecision() < this->getPrecision())
+        return ( create_obj(this->getType(), tmp) );
+    return ( create_obj(obj.getType(), tmp) );
+}
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::operator-( IOperand const & obj )
+{
+    long double tmp = std::stold(this->str_value) - std::stold(obj.toString());
+
+    if (obj.getPrecision() < this->getPrecision())
+        return ( create_obj(this->getType(), tmp) );
+    return ( create_obj(obj.getType(), tmp) );
+}
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::operator*( IOperand const & obj )
+{
+    long double tmp = std::stold(this->str_value) * std::stold(obj.toString());
+
+    if (obj.getPrecision() < this->getPrecision())
+        return ( create_obj(this->getType(), tmp) );
+    return ( create_obj(obj.getType(), tmp) );
+}
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::operator/( IOperand const & obj )
+{
+    long double tmp = std::stold(this->str_value) / std::stold(obj.toString());
+
+    if (obj.getPrecision() < this->getPrecision())
+        return ( create_obj(this->getType(), tmp) );
+    return ( create_obj(obj.getType(), tmp) );
+}
+
+template<eOperandType type, class T> IOperand const * Operand<type, T>::operator%( IOperand const & obj )
+{
+    long double tmp = fmod(std::stold(this->str_value) , std::stold(obj.toString()));
+
+    if (obj.getPrecision() < this->getPrecision())
+        return ( create_obj(this->getType(), tmp) );
+    return ( create_obj(obj.getType(), tmp) );
+}
 
 template<eOperandType type, class T> Operand<type, T>::Operand( T tmp_val ,std::string str_tmp_val )
 {
@@ -33,6 +100,16 @@ template<eOperandType type, class T> Operand<type, T>::Operand( T tmp_val ,std::
     this->num_value = tmp_val;
     std::cout << this->str_value << std::endl;
     std::cout << static_cast<int> (this->num_value) << " " << sizeof(this->num_value) << std::endl;
+}
+
+template<eOperandType type, class T> std::string const & Operand<type, T>::toString() const
+{
+    return (this->str_value);
+}
+
+template<eOperandType type, class T> int Operand<type, T>::getPrecision() const
+{
+    return (type);
 }
 
 template<eOperandType type, class T> eOperandType Operand<type, T>::getType() const
